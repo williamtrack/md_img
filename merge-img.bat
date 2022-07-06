158eb9e9f42e387e0b5d49776bcb5505a4c388e5
William <zingon@163.com>
2022-07-06 13:30:59 +0800

    20220706_133059

---
+++ merge-img.bat
@@ -0,0 +1,31 @@
+d:
+cd  D:/data/md/md_img
+@ECHO OFF
+SET year=%date:~6,4%
+SET month=%date:~0,2%
+SET day=%date:~3,2%
+SET tt=%time:~0,8%
+SET Today=%year%%month%%day%_%time:~0,2%%time:~3,2%%time:~6,2%
+rem ECHO %Today%
+rem ECHO %date%
+rem ECHO %time%
+@ECHO ON
+
+git add .
+git commit -am "%Today%"
+git remote rm origin
+git remote add origin git@github.com:williamtrack/md_img.git
+git checkout master
+git pull origin master
+pause
+
+git checkout dev
+git merge master
+pause
+
+git checkout master
+git merge dev
+pause
+
+git push
+pause
